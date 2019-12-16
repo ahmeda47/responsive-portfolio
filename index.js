022ff18498a9499489babfd6529f7074cb205e7a
Abdiwahab Ahmed <abdiwahabahmed@utmuser56-208.wireless.utoronto.ca>
2019-12-16 17:46:34 -0500

    added most of portfolio

---
+++ index.js
@@ -0,0 +1,7 @@
+//var aos = require("aos")
+$(document).ready(function(){
+    $('.menu-toggler').on('click', function(){
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    })
+})
